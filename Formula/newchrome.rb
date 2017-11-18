a6c42fe2ba61257e08fab1c473ecff0859b2da3c
Alex Lu <alxlu@users.noreply.github.com>
2017-11-18 12:36:57 -0800

    update newchrome version

--- Formula/newchrome.rb
+++ Formula/newchrome.rb
@@ -1,8 +1,8 @@
 class Newchrome < Formula
   desc "Manage chrome profiles"
   homepage "https://alx.lu"
-  url "https://github.com/alxlu/newchrome/releases/download/v0.0.1/newchrome-0.0.1.tar.gz"
-  sha256 "bf7ce4acebc370c20394bce068bc82b9e79a0ba738f775f1e406474fa6a38ca0"
+  url "https://github.com/alxlu/newchrome/releases/download/v0.0.2/newchrome-0.0.2.tar.gz"
+  sha256 "80473b2c3aedeb0e77587c2da9fde1cddb66b9b329f2e5266b8e4f3c48c89313"
 
   def install
     bin.install "newchrome"
